4b3dae06a989ee3270667f0b9736c75c4ac3d541
sadjad1366 <sadjad.ghasemi@gmail.com>
2024-09-15 15:46:58 +0430

    fix the tailwind problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content:
-   ["**/*.html","./**/*.{js,ts,jsx,tsx}"],
+  //  ["**/*.html","./**/*.{js,ts,jsx,tsx}","./home.html"],
+  ["./index.html",
+"./login.html",
+"./signup.html",
+"./home.html",
+"./search.html",
+"product-details.html",
+"./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
   },
   // plugins: [require("@tailwindcss/forms")],
 
 };
+
